316ade837e96258c42f1eaf4b3f7fd69dbc3521a
kerenp <kerenp2014@gmail.com>
2024-08-17 22:15:20 +0800

    init commit for docker

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome Keren!\n');
+  res.end('Hello World Keren\n');
 });
 
 server.listen(port, hostname, () => {
